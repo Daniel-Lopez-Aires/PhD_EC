5d17b2f1c09eff14b03e128b24ff3752be8cca5a
Daniel Lopez Aires <64276735+Daniel-Lopez-Aires@users.noreply.github.com>
2022-01-12 12:15:20 +0100

    Removed try/except and implemented C,S plot

--- Clay_selection_SKB.py
+++ Clay_selection_SKB.py
@@ -28,12 +28,22 @@ import Fits
 
 #%% ###################### 1) Data loading #########################
 
+#For the C and S analysis, since several measurements were taken, I compute the meam
+#and associate an error as (max-min)/2
+    #Note that sometinmes in the content it is say <.02, I guess this is that the apparatus
+        #can not measure it. I will write 0.02, althouhgh that means doing a bit mistake!
+    #Bulgarian data unkown to assign, all the samples says bulgaria, without specifying, so will
+        #assign it randomly, since the important thing is to plot it, is not a big problem. nevertheless,
+        #take it intro account!
 
 Milos = {'name' : "Milos", '%Mont' : 43.6, 'Delta_%Mont' : 1, 
          '%Mica' : 5.6, 'Delta_%Mica' : .5, 
          'CEC_total[cmol/kg]' : 55.6, 'Delta_CEC_total[cmol/kg]' : .6,
          'CEC_clay[cmol/kg]' : 95.7, 'Delta_CEC_clay[cmol/kg]' : .2,
          'EC[cmol/kg]' : 61.2 , 'Delta_EC[cmol/kg]': .5 ,
+         'perc_C' : (.23 + .24 + .25)/3, 'Delta_perc_C':(.25 - .23)/2, 
+         'perc_S' : (.87 + .79 + .73)/3, 'Delta_perc_S':(.87 - .73)/2, 
+         'perc_S_S2-' : (.43 + .36 + .29)/3, 'Delta_perc_S_S2-':(.43 - .29)/2, 
          'rho_grain[kg/m3]' :2606, 'Delta_rho_grain[kg/m3]' : 13,
          'rho_min[kg/m3]' : 1440, 'rho_max[kg/m3]': 1530,
          'Geo_H2O' : 17.9, 'Gen_H2O' : 15.1,
@@ -47,6 +57,9 @@ Morocco = {'name' : "Morocco", '%Mont' : 84.8, 'Delta_%Mont' : 1.48,
          'CEC_total[cmol/kg]' : 80.6, 'Delta_CEC_total[cmol/kg]' : .9,
          'CEC_clay[cmol/kg]' : 104.3, 'Delta_CEC_clay[cmol/kg]' : 0,
          'EC[cmol/kg]' : 82.3 , 'Delta_EC[cmol/kg]': 1.5 ,
+         'perc_C' : (.06 + .03 + .03)/3, 'Delta_perc_C':(.06 - .03)/2, 
+         'perc_S' : (.03 + .05 + .03)/3, 'Delta_perc_S':(.05 - .03)/2, 
+         'perc_S_S2-' : (.02 + .02 + .02)/3, 'Delta_perc_S_S2-':(.02 - .02)/2,
          'rho_grain[kg/m3]' :2737, 'Delta_rho_grain[kg/m3]' : 16,
          'rho_min[kg/m3]' : 1440, 'rho_max[kg/m3]': 1550,
          'Geo_H2O' : 17.7, 'Gen_H2O' : 15,
@@ -57,6 +70,9 @@ Turkish = {'name' : "Turkey", '%Mont' : 72.43, 'Delta_%Mont' : 1.39,
          'CEC_total[cmol/kg]' : 68.6, 'Delta_CEC_total[cmol/kg]' : .5,
          'CEC_clay[cmol/kg]' : 86, 'Delta_CEC_clay[cmol/kg]' : 1.3,
          'EC[cmol/kg]' : 93.3 , 'Delta_EC[cmol/kg]': .9 ,
+         'perc_C' : (.61 + .62 + .64)/3, 'Delta_perc_C':(.64 - .61)/2, 
+         'perc_S' : (.02 + .02 + .02)/3, 'Delta_perc_S':(.87 - .73)/2, 
+         'perc_S_S2-' : (.02 + .02 + .02)/3, 'Delta_perc_S_S2-':(.02 - .02)/2,
          'rho_grain[kg/m3]' :2605, 'Delta_rho_grain[kg/m3]' : 83,
          'rho_min[kg/m3]' : 1485, 'rho_max[kg/m3]': 1580,
          'Geo_H2O' : 9.3, 'Gen_H2O' : 8.5,
@@ -67,6 +83,9 @@ Wyoming = {'name' : "MX-80", '%Mont' : 85.3, 'Delta_%Mont' : 1.5,
          'CEC_total[cmol/kg]' : 84, 'Delta_CEC_total[cmol/kg]' : .6,
          'CEC_clay[cmol/kg]' : 0, 'Delta_CEC_clay[cmol/kg]' : 0,
          'EC[cmol/kg]' : 87.7 , 'Delta_EC[cmol/kg]': .3 ,
+         'perc_C' : (.44 + .42 + .42)/3, 'Delta_perc_C':(.44 - .42)/2, 
+         'perc_S' : (.21 + .21 + .21)/3, 'Delta_perc_S':(.21 - .21)/2, 
+         'perc_S_S2-' : (.03 + .04 + .04)/3, 'Delta_perc_S_S2-':(.04 - .03)/2,
          'rho_grain[kg/m3]' :2769, 'Delta_rho_grain[kg/m3]' : 6,
          'rho_min[kg/m3]' : 1480, 'rho_max[kg/m3]': 1585,
          'Geo_H2O' : 11.4, 'Gen_H2O' : 10.2,
@@ -77,6 +96,9 @@ Sardinian = {'name' : "Sardinia", '%Mont' : 85.2, 'Delta_%Mont' : .14,
          'CEC_total[cmol/kg]' : 108.6, 'Delta_CEC_total[cmol/kg]' : 1.6,
          'CEC_clay[cmol/kg]' : 0, 'Delta_CEC_clay[cmol/kg]' : 0,
          'EC[cmol/kg]' : 122.7 , 'Delta_EC[cmol/kg]': 2.1 ,
+         'perc_C' : (.18 + .19 + .18)/3, 'Delta_perc_C':(.19 - .18)/2, 
+         'perc_S' : (.03 + .03 + .03)/3, 'Delta_perc_S':(.03 - .03)/2, 
+         'perc_S_S2-' : (.02 + .02 + .02)/3, 'Delta_perc_S_S2-':(.02 - .02)/2,
          'rho_grain[kg/m3]' :2812, 'Delta_rho_grain[kg/m3]' : 10,
          'rho_min[kg/m3]' : 1430, 'rho_max[kg/m3]': 1510,
          'Geo_H2O' : 19.4, 'Gen_H2O' : 16.3,
@@ -87,6 +109,9 @@ Indian = {'name' : "India", '%Mont' : 77.2, 'Delta_%Mont' : .71,
          'CEC_total[cmol/kg]' : 79.8, 'Delta_CEC_total[cmol/kg]' : .05,
          'CEC_clay[cmol/kg]' : 89.4, 'Delta_CEC_clay[cmol/kg]' : .05,
          'EC[cmol/kg]' : 92.1 , 'Delta_EC[cmol/kg]': 5.1 ,
+         'perc_C' : (.14 + .14 + .15)/3, 'Delta_perc_C':(.15 - .14)/2, 
+         'perc_S' : (.14 + .15 + .15)/3, 'Delta_perc_S':(.15 - .14)/2, 
+         'perc_S_S2-' : (.02 + .02 + .02)/3, 'Delta_perc_S_S2-':(.02 - .02)/2,
          'rho_grain[kg/m3]' :2931, 'Delta_rho_grain[kg/m3]' : 19,
          'rho_min[kg/m3]' : 1420, 'rho_max[kg/m3]': 1540,
          'Geo_H2O' : 14.4, 'Gen_H2O' : 12.6,
@@ -97,6 +122,9 @@ Bulgarian_1 = {'name' : "Bulgaria$_{20kg}$", '%Mont' : 73.8, 'Delta_%Mont' : 1.7
          'CEC_total[cmol/kg]' : 73.1, 'Delta_CEC_total[cmol/kg]' : 1.1,
          'CEC_clay[cmol/kg]' : 85.3, 'Delta_CEC_clay[cmol/kg]' : 0,
          'EC[cmol/kg]' : 81.8 , 'Delta_EC[cmol/kg]': .3 ,
+         'perc_C' : (.82 + .78 + .89)/3, 'Delta_perc_C':(.89 - .78)/2, 
+         'perc_S' : (.05 + .05 + .06)/3, 'Delta_perc_S':(.06 - .05)/2, 
+         'perc_S_S2-' : (.02 + .02 + .02)/3, 'Delta_perc_S_S2-':(.02 - .02)/2,
          'rho_grain[kg/m3]' :2757, 'Delta_rho_grain[kg/m3]' : 7,
          'rho_min[kg/m3]' : 1280, 'rho_max[kg/m3]': 1400,
          'Geo_H2O' : 20.1, 'Gen_H2O' : 16.7,
@@ -107,6 +135,9 @@ Bulgarian_2 = {'name' : "Bulgaria$_{20ton}$", '%Mont' : 82.6, 'Delta_%Mont' : .5
          'CEC_total[cmol/kg]' : 79.4, 'Delta_CEC_total[cmol/kg]' : .4,
          'CEC_clay[cmol/kg]' : 0, 'Delta_CEC_clay[cmol/kg]' : 0,
          'EC[cmol/kg]' : 0 , 'Delta_EC[cmol/kg]': 0 ,
+         'perc_C' : (.58 + .86 + .59)/3, 'Delta_perc_C':(.86 - .58)/2, 
+         'perc_S' : (.03 + .04 + .03)/3, 'Delta_perc_S':(.04 - .03)/2, 
+         'perc_S_S2-' : (.02 + .02 + .02)/3, 'Delta_perc_S_S2-':(.02 - .02)/2,
          'rho_grain[kg/m3]' :2770, 'Delta_rho_grain[kg/m3]' : 6,
          'rho_min[kg/m3]' : 0, 'rho_max[kg/m3]': 0,
          'Geo_H2O' : 19.6, 'Gen_H2O' : 16.4,
@@ -118,6 +149,9 @@ Bulgarian_3 = {'name' : "Bulgaria$_F$", '%Mont' : 79.2, 'Delta_%Mont' : 1.7,
          'CEC_total[cmol/kg]' : 76.2, 'Delta_CEC_total[cmol/kg]' : .6,
          'CEC_clay[cmol/kg]' : 89.9, 'Delta_CEC_clay[cmol/kg]' : 0,
          'EC[cmol/kg]' : 83.5 , 'Delta_EC[cmol/kg]': .2 ,
+         'perc_C' : (.74 + .72 + .73)/3, 'Delta_perc_C':(.74 - .72)/2, 
+         'perc_S' : (.05 + .04 + .05)/3, 'Delta_perc_S':(.05 - .04)/2, 
+         'perc_S_S2-' : (.02 + .02 + .02)/3, 'Delta_perc_S_S2-':(.02 - .02)/2,
          'rho_grain[kg/m3]' :2758, 'Delta_rho_grain[kg/m3]' : 5,
          'rho_min[kg/m3]' : 1315, 'rho_max[kg/m3]': 1425,
          'Geo_H2O' : 18.8, 'Gen_H2O' : 15.9,
@@ -136,14 +170,20 @@ Bentonites = np.array( [Milos, Morocco, Indian, Sardinian, Turkish, Wyoming, Bul
         
 names = np.array([])
 CEC_total = np.array([])                        #storing of the CEC_toal [cmol/kg]
-Delta_CEC_total = np.array([])      #storing of the error of CEC_toal [cmol/kg]
+Delta_CEC_total = np.array([])          #storing of the error of CEC_toal [cmol/kg]
 CEC_clay = np.array([])                        #storing of the CEC_toal [cmol/kg]
-Delta_CEC_clay = np.array([])      #storing of the error of CEC_toal [cmol/kg]
+Delta_CEC_clay = np.array([])        #storing of the error of CEC_toal [cmol/kg]
 EC = np.array([])                        #storing of the EC [cmol/kg]
-Delta_EC = np.array([])      #storing of the error of EC [cmol/kg]
-mineral_type = np.array([])         #type of mineral, smectite or montmorillonite'
-mineral_wt = np.array([])           #content of that mineral
-Delta_mineral_wt = np.array([])           #error of the mineral content
+Delta_EC = np.array([])                 #storing of the error of EC [cmol/kg]
+mineral_wt = np.array([])                   #content of that mineral
+Delta_mineral_wt = np.array([])             #error of the mineral content
+perc_C = np.array([])                               #%C
+perc_S = np.array([])                               #%S
+perc_S_S2 = np.array([])                            #%S/S2-
+Delta_perc_C = np.array([])                         #error of %C
+Delta_perc_S = np.array([])                         #error of %S
+Delta_perc_S_S2 = np.array([])                      #error of %S/S2-
+
 rho_grain = np.array([])            #grain density [kg/m3]
 Delta_rho_grain = np.array([])#error of the grain density
 rho_min = np.array([])              #Lower limit of rho [kg/m3]
@@ -160,6 +200,14 @@ for i in range(0, len(Bentonites)):
     Delta_CEC_clay = np.append(Delta_CEC_clay, Bentonites[i]['Delta_CEC_clay[cmol/kg]'])
     EC = np.append(EC, Bentonites[i]['EC[cmol/kg]'])
     Delta_EC = np.append(Delta_EC, Bentonites[i]['Delta_EC[cmol/kg]'])
+    
+    perc_C = np.append(perc_C, Bentonites[i]['perc_C'])
+    perc_S = np.append(perc_S, Bentonites[i]['perc_S'])
+    perc_S_S2 = np.append(perc_S_S2, Bentonites[i]['perc_S_S2-'])
+    Delta_perc_C = np.append(Delta_perc_C, Bentonites[i]['Delta_perc_C'])
+    Delta_perc_S = np.append(Delta_perc_S, Bentonites[i]['Delta_perc_S'])
+    Delta_perc_S_S2 = np.append(Delta_perc_S_S2, Bentonites[i]['Delta_perc_S_S2-'])
+    
     rho_grain = np.append(rho_grain, Bentonites[i]['rho_grain[kg/m3]'])
     Delta_rho_grain = np.append(Delta_rho_grain, Bentonites[i]['Delta_rho_grain[kg/m3]'])
     rho_min = np.append(rho_min, Bentonites[i]['rho_min[kg/m3]'])
@@ -167,16 +215,9 @@ for i in range(0, len(Bentonites)):
     Gen_H2O = np.append(Gen_H2O, Bentonites[i]['Gen_H2O'])
     Geo_H2O = np.append(Geo_H2O, Bentonites[i]['Geo_H2O'])
     
-    try: #If the mineral is montorillonite
-        mineral_wt = np.append(mineral_wt,Bentonites[i]['%Mont'] )
-        Delta_mineral_wt = np.append(Delta_mineral_wt, Bentonites[i]['Delta_%Mont'])     
-        mineral_type = np.append(mineral_type, 'Mont')
-  
-    except: #the mineral is smectite (No longer used, since is Montmorillonite always!)
-        mineral_wt = np.append(mineral_wt,Bentonites[i]['%Smec'] )
-        Delta_mineral_wt = np.append(Delta_mineral_wt, Bentonites[i]['Delta_%Smec'])    
-        mineral_type = np.append(Delta_mineral_wt, 'Smectite')
-        
+    mineral_wt = np.append(mineral_wt,Bentonites[i]['%Mont'] )
+    Delta_mineral_wt = np.append(Delta_mineral_wt, Bentonites[i]['Delta_%Mont'])     
+
         
         
 #%% ################################ 3) Data plotting #######################
@@ -231,6 +272,23 @@ plt.grid(True)
 plt.xticks(rotation=30) #rotation = 'vertical', 40 for 40 degress
 plt.savefig('Mont_content_vs_bentonite.png', format='png')
 
+### C and S content
+
+X_axis = np.arange(len(names))                 #To do the 2 bar plot
+w = .3          #bar width
+
+plt.figure(figsize=(10,8))  #width, heigh 6.4*4.8 inches by default
+plt.title("C and S content", fontsize=22, wrap=True)           #title
+plt.bar(X_axis - w, perc_C, yerr = Delta_perc_C, width = 1*w, edgecolor="black", label = '%C', align='center') 
+plt.bar(X_axis , perc_S, yerr = Delta_perc_S, width = 1*w, edgecolor="black", label = '%S', align='center') 
+plt.bar(X_axis + w, perc_S_S2, yerr = Delta_perc_S_S2,  width = 1*w, edgecolor="black", label = '%S/S$^{2-}$', align='center') 
+plt.ylabel("Impurity content", fontsize=14)              #ylabel
+plt.xlabel('Bentonite', fontsize = 14)
+plt.tick_params(axis='both', labelsize=14)              #size of axis
+plt.grid(True)
+plt.legend()
+plt.xticks(X_axis, names, rotation=30) #rotation = 'vertical', 40 for 40 degress
+plt.savefig('C_S_content_vs_bentonite.png', format='png')
 
 ################ Physical properties ###########
 
@@ -289,9 +347,5 @@ plt.savefig('H2O_gen_vs_bentonite.png', format='png')
 #%% ############### 4) Discussion ############################
 
 '''
-No big differences in the self made plots. Neither in the created y the author.
 
-TO plot:
-    .Limits in density fulfilling Pswell 
-    .extract graphs from te report and make slides
 '''
